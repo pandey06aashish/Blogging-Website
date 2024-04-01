13e29151e312897a967d63b788783fd3195814ce
pandey06aashish <pandey786aashish@gmail.com>
2024-04-01 17:10:01 +0530

    pages and route

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,3 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signout from "./pages/Signout";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+
 export default function App() {
-  return <h1 className="text-3xl text-red-500">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<Signout />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/project" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
