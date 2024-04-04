cdb9c7982e86831a6f17565f1711ce39cb9f4125
pandey06aashish <pandey786aashish@gmail.com>
2024-04-04 06:27:26 +0530

    createing header section

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -5,17 +5,19 @@ import Signout from "./pages/Signout";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-out" element={<Signout />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/project" element={<Projects />} />
+        <Route path="/project" element={<Projects/>} />
       </Routes>
     </BrowserRouter>
   );
